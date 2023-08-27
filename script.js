21b10a399252cc8e1cdf7859a7f4c70c68b55940
H-akim <92632018+H-akim@users.noreply.github.com>
2023-08-27 13:11:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById("menu");
+var menu2 = document.getElementById("menu2");
+
+menu.addEventListener("click", ()=>{
+    menu.classList.toggle("openmenu");
+})
+menu_2.addEventListener("click", ()=>{
+    menu_2.classList.toggle("openmenu_2");
+})
